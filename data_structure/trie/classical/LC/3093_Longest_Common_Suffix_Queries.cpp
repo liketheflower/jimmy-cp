10baddf8532ddfffc6cdbe2460c6110e99168ce1
jimmy shen <jimmy@takemobi.com>
2024-03-24 00:50:24 -0400

    Add a trie solution

---
+++ data_structure/trie/classical/LC/3093_Longest_Common_Suffix_Queries.cpp
@@ -0,0 +1,89 @@
+// https://leetcode.com/problems/longest-common-suffix-queries/description/
+const int INF= 1e6;
+struct vertex {
+    int idx;
+    int len;
+    unordered_map<int, vertex*> child;
+    vertex(){
+        idx = INF;
+        len = INF;
+    }
+};
+class Trie {
+public:
+    vertex* root;
+    Trie() {
+        root = new vertex();
+    }
+    void insert(string word, int idx) {
+        vertex* curr = root;
+        int this_len = word.size();
+        if (this_len < curr -> len){
+            curr -> idx = idx;
+            curr -> len = this_len;
+        }
+        if (this_len == curr -> len){
+            curr -> idx = min(idx, curr -> idx);
+        }
+        for (int i = 0; i < (int)word.size(); ++i) {
+            int alphaNum = word[i] - 'a';
+            if (curr->child.find(alphaNum) == curr->child.end()) {
+                curr->child[alphaNum] = new vertex();
+            }
+            curr = curr->child[alphaNum];
+            if (this_len < curr -> len){
+                curr -> idx = idx;
+                curr -> len = this_len;
+            }
+            if (this_len == curr -> len){
+                curr -> idx = min(idx, curr -> idx);
+            }   
+        }
+    }
+
+};
+
+class Solution {
+public:
+    void  reverse_s(vector<string>& words){
+        int n = words.size();
+        for (int i = 0; i < n;++i){
+            reverse(words[i].begin(), words[i].end());
+        }
+    }
+
+    int find_ret(string &word, vertex* curr){
+        vector<string> ret;
+        int i = 0;
+        int n = word.size();
+        while (true){
+            if (i == n){
+                return curr->idx;
+            }
+            int idx = word[i] - 'a';
+            if (curr->child.find(idx)== curr->child.end()){
+                return curr -> idx;
+            } else {
+                curr = curr->child[idx];
+                i++;
+            }
+        }
+        return 0;
+    }
+    vector<int> stringIndices(vector<string>& words, vector<string>& words2) {
+        Trie trie;
+        reverse_s(words);
+        reverse_s(words2);
+        const int n = words.size();
+        for (int i = 0; i < n; ++i){
+            trie.insert(words[i], i);
+        }
+        const int m = words2.size();
+        vector<int> ret(m, 0);
+        auto root = trie.root;
+        for (int i = 0; i < m; ++i){
+            ret[i] = find_ret(words2[i], root);
+        }
+        return ret;
+    }
+};
